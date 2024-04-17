f466d0c49ef58b74b719248ff8017f18097b72a9
Jorge Suazo <jsuazof@gmail.com>
2024-04-17 11:34:32 -0400

    Java files

--- JavaExercises/IndiceSalud.java
+++ JavaExercises/IndiceSalud.java
@@ -2,6 +2,20 @@ package JavaExercises;
 
 public class IndiceSalud {
 
+    public static void main(String[] args) {
+    
     Persona persona1 = new Persona();
+
+    persona1.rut="12.156.359-8";
+    persona1.nombre="Juanito Pérez";
+    persona1.edad=18;
+    persona1.altura=(float)1.80;
+    persona1.peso=(float)70;
+    //persona1.Verificar();
+
+    persona1.Clasificacion();
+    
+
+    }
 
 }
--- JavaExercises/Persona.java
+++ JavaExercises/Persona.java
@@ -1,7 +1,6 @@
 package JavaExercises;
 
-import java.math.BigDecimal;
-import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Persona {
 
@@ -11,11 +10,59 @@ public class Persona {
     float altura;
     float peso;
 
-
-        public void IMC()
-    {
+    public float IMC() {
         float imc;
-        imc = (float) (peso / Math.pow(altura,2));
+        imc = (float) (peso / Math.pow(altura, 2));
         System.out.println("El IMC es: " + imc);
+        return imc;
+    }
+
+    public void Verificar() {
+        System.out.println("RUT: " + rut);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad + " Años");
+        System.out.println("Altura: " + altura + " Mts");
+        System.out.println("Peso: " + peso + " Kg");
+
+    }
+
+    public void Clasificacion() {
+        float imc = IMC();
+
+        if (imc < 16) {
+            System.out.println("INFRAPESO: Delgadez Severa");
+        } else if (imc >= 16 && imc < 17) {
+            System.out.println("INFRAPESO: Delgadez Moderada");
+
+        } else if (imc >= 17 && imc < 18.5) {
+            System.out.println("INFRAPESO: Delgadez Aceptable");
+        } else if (imc >= 18.5 && imc < 25) {
+            System.out.println("Peso Normal");
+        } else if (imc >= 25 && imc < 30) {
+            System.out.println("Sobrepeso");
+        } else if (imc >= 30 && imc < 35) {
+            System.out.println("OBESO: Tipo I");
+        } else if (imc >= 35 && imc < 40) {
+            System.out.println("OBESO: Tipo 2");
+        } else {
+            System.out.println("OBESO: Tipo 3");
+        }
+    }
+
+    public void menu()
+    {
+        Persona persona = new Persona();
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el RUT");
+        persona.rut = leer.nextLine();
+        System.out.println("Ingrese el nombre");
+        persona.nombre = leer.nextLine();
+        System.out.println("Ingrese la edad");
+        persona.edad = leer.nextShort();
+        System.out.println("Ingrese la altura");
+        persona.altura = leer.nextFloat();
+        System.out.println("Ingrese el peso");
+        persona.peso = leer.nextFloat();
+        leer.close();
     }
 }
